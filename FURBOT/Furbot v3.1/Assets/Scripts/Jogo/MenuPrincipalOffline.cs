0a8a4e5da8e376cd859b93fb31cc93227243c1e5
Dalton Solano dos Reis <dalton.reis@gmail.com>
2020-05-12 12:37:08 -0300

    Versão no Unity (GIT)
    Versão do Unity nuvem.

---
+++ FURBOT/Furbot v3.1/Assets/Scripts/Jogo/MenuPrincipalOffline.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuPrincipalOffline : MonoBehaviour
+{
+
+    //Objetos
+    public Button btnIniciar, btnSair, gerador;
+
+    void Start()
+    {
+#if !UNITY_WEBGL
+        btnSair.onClick.AddListener(delegate { Sair(); });
+#else
+        btnSair.gameObject.SetActive(false);
+#endif
+        btnIniciar.onClick.AddListener(delegate { Iniciar(); });
+       
+        gerador.onClick.AddListener(delegate { SceneManager.LoadScene("GeradorDeFases"); });
+    }
+
+    /// <summary>
+    /// Este método carrega a cena de seleção de fases.
+    /// </summary>
+    private void Iniciar()
+    {
+        //#if UNITY_STANDALONE
+        //SceneManager.LoadScene("Historia");
+        //#else 
+        SceneManager.LoadScene("TutorialProjecao");
+        //#endif
+    }
+
+    /// <summary>
+    /// Este método finaliza a execução do jogo.
+    /// </summary>
+    private void Sair()
+    {
+        Application.Quit();
+    }
+}
